867598d3079ba3a623050775d54068160436aee8
ghadeer <ghador377@gmail.com>
2026-04-06 23:23:30 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+import { supabase } from './supabase.js';
+
+export async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('email', email)
+    .eq('password', password)
+    .single();
+
+  if (error || !data) {
+    alert("خطأ في تسجيل الدخول");
+    return;
+  }
+
+  if (data.role === 'admin') {
+    window.location.href = "admin.html";
+  } else if (data.role === 'trainer') {
+    window.location.href = "trainer.html";
+  } else {
+    window.location.href = "trainee.html";
+  }
+}
