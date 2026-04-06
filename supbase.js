16f6253924e5e1d65eca7533dbf146ca90d3bfd8
ghadeer <ghador377@gmail.com>
2026-04-06 23:17:48 +0300

    Add files via upload

---
+++ supbase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+const supabase = createClient(
+  "https://mcxdjisoyqdnyieboqpy.supabase.co/",
+  "sb_publishable_CoPFxO5-flUjMYi-jOdikw_kwAaUoZr"
+);
