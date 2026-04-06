9dd98b8162b728fcd5a03168917e49d71d92653d
ghadeer <ghador377@gmail.com>
2026-04-07 00:17:26 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-export const supabase = createClient(
-  "https://mcxdjisoyqdnyieboqpy.supabase.co",
-  "sb-publishable-CoPFxO5-flUjMYi-jOdikw_kwAaUoZr"
-);
