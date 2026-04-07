49ed0b7a18c4ab07342563a1a3b97a474b7b17e2
ghadeer <ghador377@gmail.com>
2026-04-07 12:12:39 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,29 @@
+import { supabase } from './supabase.js';
+
+async function login() {
+  const email = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('email', email)
+    .eq('password', password)
+    .single();
+
+  if (error || !data) {
+    alert("خطأ في تسجيل الدخول");
+    return;
+  }
+
+  if (data.role === 'admin') {
+    window.location.href = "admin.html";
+  } else if (data.role === 'trainer') {
+    window.location.href = "teachers_main_page.html";
+  } else {
+    window.location.href = "student_main_page.html";
+  }
+}
+
+// مهم جداً:
+window.login = login;
---
+++ supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from 'https://mcxdjisoyqdnyieboqpy.supabase.co';
+
+const supabase = createClient(
+  "sb_publishable_CoPFxO5-flUjMYi-jOdikw_kwAaUoZr"
+);
