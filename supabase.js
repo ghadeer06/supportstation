6f25841e7f2724a830dc8786bcc2f4d6f29ab5a7
ghadeer <ghador377@gmail.com>
2026-04-07 12:59:54 +0300

    Add console log for supabase.js loading
    Added a console log to indicate that supabase.js has been loaded.

--- supabase.js
+++ supabase.js
@@ -3,3 +3,4 @@ import { createClient } from 'https://mcxdjisoyqdnyieboqpy.supabase.co';
 const supabase = createClient(
   "sb_publishable_CoPFxO5-flUjMYi-jOdikw_kwAaUoZr"
 );
+console.log("supabase.js loaded");
